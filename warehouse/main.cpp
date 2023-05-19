5511216b8590b3c87cf376e26e75cda160c4ba04
dogederp <41737008+dogederp@users.noreply.github.com>
2023-05-19 19:49:46 +0200

    cpp files toegevoegd voor het definen van functies

--- warehouse/main.cpp
+++ warehouse/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include "src/include/employee.hpp"
+#include "src/include/shelf.hpp"
+#include "src/include/warehouse.hpp"
+
+
+
 
 int main(void){
     std::cout << "Hello world" << std::endl;
--- warehouse/test/test.cpp
+++ warehouse/test/test.cpp
@@ -2,3 +2,5 @@
 #include "include/catch.hpp"
 
 // Find all tests in test*.cpp in test/
+// TODO: make testcase for Warehouse::pickItems in testWarehouse
+// TODO: Zorg ervoor dat de testcases voor rearrangeShelf werken
