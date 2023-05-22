da718f4aa28775f113f3376aaa4f343c679fc17c
dogederp <41737008+dogederp@users.noreply.github.com>
2023-05-22 15:28:40 +0200

    functies warehouse

--- warehouse/src/warehouse.cpp
+++ warehouse/src/warehouse.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <vector>
 
-#include "employee.hpp"
-#include "shelf.hpp"
-#include "warehouse.hpp"
+#include "include/employee.hpp"
+#include "include/shelf.hpp"
+#include "include/warehouse.hpp"
 
 Warehouse::Warehouse() {
     this->employees = employees;
@@ -17,4 +17,37 @@ void Warehouse::addEmployee(Employee employee) {
 
 void Warehouse::addShelf(Shelf shelf) {
     shelves.push_back(shelf);
+}
+
+bool Warehouse::rearrangeShelf(Shelf& shelf) {
+    if (shelf.isEmpty()) {
+        return false;
+    }
+    for (int i = 0; i < shelf.pallets.size(); i++) {
+        if (shelf.pallets[i].isEmpty()) {
+            for (int j = i + 1; j < shelf.pallets.size(); j++) {
+                if (!shelf.pallets[j].isEmpty()) {
+                    shelf.swapPallet(i, j);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool Warehouse::pickItems(std::string itemName, int itemCount) {
+    for (int i = 0; i < shelves.size(); i++) {
+        for (int j = 0; j < shelves[i].pallets.size(); j++) {
+            if (shelves[i].pallets[j].getItemName() == itemName) {
+                if (shelves[i].pallets[j].getItemCount() >= itemCount) {
+                    for (int k = 0; k < itemCount; k++) {
+                        shelves[i].pallets[j].takeOne();
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
