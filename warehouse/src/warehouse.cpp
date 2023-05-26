d343e91afb95a6bfb25dfdea2e52da8c206e5b49
dogederp <41737008+dogederp@users.noreply.github.com>
2023-05-26 23:42:31 +0200

    comments en een kleine code edit

--- warehouse/src/warehouse.cpp
+++ warehouse/src/warehouse.cpp
@@ -31,22 +31,26 @@ bool Warehouse::rearrangeShelf(Shelf& shelf) {
         return true;
     }
     if (shelf.isFull()) {
+        // als de shelf vol is, return true, dit telt als gesorteerd
         return true;
     }
     // check of er een qualified employee is
     for (unsigned int i = 0; i < employees.size(); i++) {
-        if (employees[i].getForkliftCertificate() == true) {
+        // Check of er iemand met een certificate beschikbaar is
+        if (employees[i].getForkliftCertificate() == true && qualifiedEmployee == false) {
             if (employees[i].getBusy() == false) {
-                employees[i].setBusy(true);
+                employees[i].setBusy(true);  // Zet geselecteerde employee op busy
                 qualifiedEmployee = true;
             }
         }
     }
     if (qualifiedEmployee == false) {
+        // Return false als er niemand met een certificate beschikbaar is
         return false;
     }
     for (unsigned int i = 0; i < shelf.pallets.size(); i++) {
         for (unsigned int j = 0; j < shelf.pallets.size() - 1; j++) {
+            // Vergelijk pallets met elkaar en sorteer van minst naar meest
             if (shelf.pallets[j].getItemCount() > shelf.pallets[j + 1].getItemCount()) {
                 shelf.swapPallet(j, j + 1);
             }
@@ -70,10 +74,11 @@ bool Warehouse::pickItems(std::string itemName, int itemCount) {
     // als er geen items meer nodig zijn, return true
     bool itemsNeeded = true;
     bool pallet = true;
-    int itemsTaken = 0;
+    int itemsTaken = 0;  // Houd bij hoeveel items er genomen worden
     for (unsigned int i = 0; i < shelves.size(); i++) {
         for (unsigned int j = 0; j < shelves[i].pallets.size(); j++) {
             pallet = true;
+            // Neem aantal van gewenste item
             if (shelves[i].pallets[j].getItemName() == itemName) {
                 while (itemsNeeded == true && pallet == true) {
                     if (shelves[i].pallets[j].getItemCount() > 0) {
@@ -82,19 +87,22 @@ bool Warehouse::pickItems(std::string itemName, int itemCount) {
                         itemCount--;
                     }
                     if (shelves[i].pallets[j].getItemCount() == 0) {
+                        // stop als er geen items meer over zijn
                         pallet = false;
                     }
                     if (itemCount == 0) {
+                        // geen items meer nodig uit magazijn
                         itemsNeeded = false;
                     }
                 }
             }
         }
     }
-    if (itemCount == 0) {
+    if (itemCount == 0) {  // Had ook if (itemsNeeded == false) kunnen zijn
         return true;
     }
     else {
+        // Geef aantal items weer die wel gepakt zijn
         std::cout << "Total maximum items taken: " << itemsTaken << std::endl;
         return false;
     }
